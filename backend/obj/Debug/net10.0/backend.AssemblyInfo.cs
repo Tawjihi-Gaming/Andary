56f92ba038fa609fab1345e2b1c6fc547ec8179f
ramasham <rsham@rama.localdomain>
2026-02-17 16:05:16 +0300

    added session approach for the anonymous players, added ready flag for the players, added room name, added endpoint for auto join the player who created the room, and fixed the score and xp

--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dec0248c603577ec7683bcaf380fb5948061574e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+891065fdf40b7c665046f84c776385aac9e12eb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
